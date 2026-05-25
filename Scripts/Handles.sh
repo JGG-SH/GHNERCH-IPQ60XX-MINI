9c717addf67afe871a48d0ec5a22f9659200cc88
ZJGG <130850482+JGG-SH@users.noreply.github.com>
2026-05-26 02:38:27 +0800

    Update argon theme configuration in Handles.sh
    Modify argon theme font and color settings in Handles.sh.

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -24,6 +24,15 @@ PKG_PATH="$GITHUB_WORKSPACE/$WRT_DIR/package/"
 # 	cd $PKG_PATH && echo "homeproxy date has been updated!"
 # fi
 
+#修改argon主题字体和颜色
+if [ -d *"luci-theme-argon"* ]; then
+	echo " " && cd ./luci-theme-argon/
+
+	sed -i "s/primary '.*'/primary '#483d8b'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+
+	cd $PKG_PATH && echo "theme-argon has been fixed!"
+fi
+
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
 if [ -f "$NSS_DRV" ]; then
